23adc33fd38ff997445422ef0120d653abac73a6
Madison Szittai <madisonszittai@madisons-mbp-2.devices.brown.edu>
2019-11-20 11:19:19 -0500

    change to 3

--- sentinalysis.py
+++ sentinalysis.py
@@ -12,23 +12,30 @@ text_file.close()
 
 analyzer = SentimentIntensityAnalyzer()
 
-
-prev_pos = 0
-prev_neg = 0
+#
+# prev_pos = 0
+# prev_neg = 0
+# prev_neu = 0
 
 print('{')
 for line in cleaned:
     vs = analyzer.polarity_scores(line)
-    cur_p = 0
-    cur_n = 0
-    if vs['neu'] == 1:
-        cur_p = prev_pos/2
-        cur_n = prev_neg/2
-    else:
-        cur_p = vs['pos']
-        cur_n = vs['neg']
-    print('{' + str(int(cur_p*180)) + ',' + str(int(cur_n*120)) + '},')
-    prev_pos = cur_p
-    prev_neg = cur_n
+    # cur_p = 0
+    # cur_n = 0
+    # cur_u = 0
+    # if vs['neu'] == 1:
+    #     cur_p = prev_pos/2
+    #     cur_n = prev_neg/2
+    # else:
+    cur_p = vs['pos']
+    cur_n = vs['neg']
+    cur_u = vs['neu']
+    # print('{' + str(int(cur_p*180)) + ',' + str(int(cur_n*120)) + '},')
+    print('{' + str(int(cur_p*100)) + ',' + str(int(cur_n*100)) + ',' + str(int(cur_u*100)) +'},')
+
+    # prev_pos = cur_p
+    # prev_neg = cur_n
+    # prev_neu = cur_u
+    #
 
 print('}')
