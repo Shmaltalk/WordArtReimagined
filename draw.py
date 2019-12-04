aff2595e31eecc667975215204fdc040d8b0057d
ShmalTalk <talie_massachi@brown.edu>
2019-12-04 17:58:45 -0500

    update pattern drawing conditions

--- draw.py
+++ draw.py
@@ -6,20 +6,16 @@ from sentinalysis import get_sentinalysis
 from line_patterns import pattern2num, num2pattern
 from points import draw_pattern
 
-colors = ["red", "blue", "green", "yellow"] # temp
-
 def draw():
     sentinalysis_values = get_sentinalysis()
+    print(sentinalysis_values)
     pattern_list = []
+
     for tup in sentinalysis_values:
-        pattern_list.append(get_pattern(tup))
+        curr_pattern = get_pattern(tup)
+        pattern_list.append(curr_pattern)
 
     print(pattern_list)
-
-    root = tk.Tk() # temp
-
-    size = 800, 500 # temp
-    #draw = aggdraw.Dib("RGB", size) # temp
     w = tk.Canvas(root, width=size[0], height=size[1])
     w.pack()
 
@@ -27,25 +23,19 @@ def draw():
     i_dir = 1
     j = 200
     for pat, mult in pattern_list:
-        if i >= 500 or i<=100: # at horizontal edges
+        i_temp = i + (10 * i_dir * mult)
+        if i_temp >= 500 or i_temp<=100: # at horizontal edges
             i_dir *= -1 # switch drawing directions
-            j += 20 # increment y val
+            j += 40 # increment y val
         xy = draw_pattern(pat, mult, i_dir, i, j)
         i += 10 * i_dir * mult
-        w.create_line(xy) # temp
-
-    #frame = tk.Frame(root, width=size[0], height=size[1], bg="") # temp
-    #frame.bind("<Expose>", lambda e: draw.expose(hwnd=e.widget.winfo_id())) # temp
-    #frame.pack() # temp
-
-    tk.mainloop() # temp
 
 def draw_pattern(patternNum, mult, i_dir, i, j):
     pattern = num2pattern[patternNum]
     xy = []
     for p in pattern:
         xy.append(p[0] * i_dir * mult + i)
-        xy.append(p[1] * i_dir * mult + j)
+        xy.append(p[1] * mult + j)
     return xy
 
 def get_pattern(tup):
@@ -53,20 +43,27 @@ def get_pattern(tup):
     neg = tup[1]
     neu = tup[2]
 
-    return (random.randint(0, 5), random.random()*4 + 7)
-    # if (pos > neg):
-    #     if (neg >= neu):
-    #         return (pattern2num["loop"], 6+((pos-neg)*5)) # pos > neg >= neu
-    #     elif (pos >= neu): # pos >= neu > neg
-    #         return (pattern2num["double_loop"], 6+((pos-neu)*5))
-    #     else: # neu > pos > neg
-    #         return (pattern2num["flat"], 6+((neu-pos)*5))
-    # else:
-    #     if (pos >= neu): # neg >= pos >= neu
-    #         return (pattern2num["madisons_stupid_line"], 6+((neg-pos)*5))
-    #     elif (neg >= neu): # neg >= neu > pos
-    #         return (pattern2num["arc"], 6+((neg-neu)*5))
-    #     else: # neu > neg >= pos
-    #         return (pattern2num["wavy_line"], 6+((neu-neg)*5))
+    #return (random.randint(0, 5), random.random()*2 + 4)
+    size_ratio = 7
+    if (neu == 1):
+        return (pattern2num["flat"], 3)
+    elif (neu > .75):
+        return (pattern2num["wavy_line"], (neu*size_ratio))
+    elif (pos < .25):
+        if (neg < .25):
+            return (pattern2num["arc"], 3)
+        else:
+            if (neu>neg):
+                return (pattern2num["madisons_stupid_line_updwn"], (neg*size_ratio))
+            else:
+                return (pattern2num["madisons_stupid_line"], (neg*size_ratio))
+    else:
+        if (neg < .25):
+            if (neu>pos):
+                return (pattern2num["double_loop_updwn"], (pos*size_ratio))
+            else:
+                return (pattern2num["double_loop"], (pos*size_ratio))
+        else:
+            return (pattern2num["loop"], (max(pos, neg)*size_ratio))
 
 draw()
